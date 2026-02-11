c6d7e3422df5f036c241d4d73bef5cf6eeba7ad6
SirBepy <tecnomon99@gmail.com>
2026-02-11 18:10:06 +0100

    FIX: stop using depricated imports

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import './styles/main.scss'
+import './styles/sections/_landing.scss'
+import './styles/sections/_details.scss'
+import './styles/sections/_timeline.scss'
+import './styles/sections/_faqs.scss'
+import './styles/sections/_rsvp.scss'
+// import './styles/sections/_envelope.scss'
 
 import { landing } from './sections/landing.js'
 import { details } from './sections/details.js'
