a3379da8518cec8df75749db760f3470e1b0dd16
SirBepy <tecnomon99@gmail.com>
2026-02-11 17:58:20 +0100

    FEAT: initial content for landing page

--- src/sections/landing.js
+++ src/sections/landing.js
@@ -1,7 +1,20 @@
 export function landing() {
   return `
     <section id="landing" class="section">
-      <h1 class="section-title">Landing Page</h1>
+      <div class="landing-content">
+        <h1 class="section-title">You're Invited</h1>
+        <p class="font-text landing-text">To the wedding of</p>
+        <p class="font-decorative3 landing-names">Josip & Storm</p>
+        <p class="font-text landing-text">On</p>
+        <p class="font-text landing-text landing-text--faded">Saturday, September 12th</p>
+        <!-- TODO: link to calendar -->
+        <p class="font-decorative3 landing-date">12.09.2026</p>
+        <div class="landing-location">
+          <img src="/icons/location.svg" alt="Location" class="landing-location__icon" />
+          <p class="landing-location__text">Hotel Storia<br/>Tasovčići bb, 88300, Bosnia & Herzegovina</p>
+        </div>
+        <p class="font-text landing-rsvp">RSVP by August 29th</p>
+      </div>
     </section>
   `
 }
