f7216a174e99f51866afced4e63e5a23f0ea32a0
SirBepy <tecnomon99@gmail.com>
2026-02-23 23:42:19 +0100

    FEAT: Add todos for tomorrow

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,11 @@ import "./styles/main.scss";
 history.scrollRestoration = "manual";
 
 if (window.location.hash) {
-  history.replaceState(null, "", window.location.pathname + window.location.search);
+  history.replaceState(
+    null,
+    "",
+    window.location.pathname + window.location.search,
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("app")).render(
@@ -14,3 +18,7 @@ ReactDOM.createRoot(document.getElementById("app")).render(
     <App />
   </React.StrictMode>,
 );
+
+// TODO: Constantly rotating flowers in the background
+// TODO: Writing svg thingy
+// TODO: As the envelope falls, already start resizing the card
