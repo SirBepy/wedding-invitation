bfc511ed74750e241bf0e99a5a7400262d32f8ae
SirBepy <tecnomon99@gmail.com>
2026-03-10 23:17:29 +0100

    CORE: Bold questions in faq

--- src/sections/Faqs/Faqs.jsx
+++ src/sections/Faqs/Faqs.jsx
@@ -6,15 +6,15 @@ const faqCategories = [
     name: "General",
     questions: [
       {
-        q: "Can I bring a plus one?",
+        q: "Can I bring a <b>plus one</b>?",
         a: "Please check with us on WhatsApp before you RSVP to confirm if you can bring a plus one.",
       },
       {
-        q: "Will there be transportation available while in Medugorje?",
+        q: "Will there be <b>transportation</b> available while in Medugorje?",
         a: "We will organise a shuttle bus for the day of the wedding for any guests who do not have access to a car.",
       },
       {
-        q: "Is accommodation available at Hotel Storia?",
+        q: "Is <b>accommodation</b> available at Hotel Storia?",
         a: "Yes! You can book a room at Hotel Storia through <a href='https://us2.cloudbeds.com/en/reservation/pDz0Sr?mode=popup&currency=bam' target='_blank'>this link</a>, using the code \"<button class='copy-code' data-copy='120926'>120926</button>\" to get the wedding discount for your accomodation!<br /><br />Please note: You <b>can't book the 13th</b> since the hotel is already booked out that day.",
       },
     ],
@@ -23,19 +23,19 @@ const faqCategories = [
     name: "Travel",
     questions: [
       {
-        q: "Do I need a visa to get to Medugorje?",
+        q: "Do I need a <b>Visa</b> to get to Medugorje?",
         a: "UK and Canadian guests do not require a visa. For <b>South Africans</b>, please note that you will need a <b>multi-entry Schengen visa</b>. Additionally, you need to enter BiH from a Schengen country (we recommend Croatia).",
       },
       {
-        q: "Which airport do I fly to?",
+        q: "Which <b>airport</b> do I fly to?",
         a: "If you're wanting to fly directly to Croatia, we'd recommend the <b>Split</b> airport as it is the closet to Medugorje - 2 hours drive away. You can always fly through Zagreb, but Zagreb airport is 5-6 hours drive away.",
       },
       {
-        q: "If I fly through Split, how do I get to Medugorje?",
+        q: "If I fly through <b>Split</b>, how do I get <b>to Medugorje</b>?",
         a: "You can catch a bus from Split to Medugorje. If there are enough SA guests coming through Split, we can try to organise a shuttle bus to pick the lot of you up and bring you to Medugorje.",
       },
       {
-        q: "What touristic activities can I do while in Croatia and Hercegovina?",
+        q: "What <b>touristic activities</b> can I do while in Croatia and Hercegovina?",
         a: `Please take a look at <a href='Places to Visit while Travelling to Medj.pdf' target='_blank'>this document</a> that lists our recommendations for touristic activities in Croatia and Hercegovina.`,
       },
     ],
@@ -119,7 +119,7 @@ export default function Faqs() {
                         className="faqs-questions__question font-text"
                         onClick={() => toggleQuestion(faq.q)}
                       >
-                        <span>{faq.q}</span>
+                        <span dangerouslySetInnerHTML={{ __html: faq.q }} />
                         <span className="faqs-questions__arrow"></span>
                       </button>
                       {isOpen && (
